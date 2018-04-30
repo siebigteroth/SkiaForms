d900ecae3bc7b4015431f0c4c5e33d74e1a1743f
Tim Siebigteroth <tim@siebigteroth.me>
2018-04-30 12:50:49 +0200

    Init

---
+++ samples/Samples/Samples.Gtk2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.GTK;
+using Xamarin.Forms.Platform.GTK.Helpers;
+
+namespace Samples.Gtk2
+{
+    public class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            GtkThemes.Init();
+            if (PlatformHelper.GetGTKPlatform() == GTKPlatform.Windows)
+            {
+                GtkThemes.LoadCustomTheme("Themes/gtkrc-dark");
+            }
+
+            Gtk.Application.Init();
+            Forms.Init();
+            var app = new App();
+            var window = new FormsWindow();
+            window.LoadApplication(app);
+            window.SetApplicationTitle("GTK2 Samples");
+            window.Show();
+            Gtk.Application.Run();
+        }
+    }
+}
